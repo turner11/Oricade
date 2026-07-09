e32c18d2c249bd2785dff3bb1a93d72dcb6f6d8b
Avi Turner <avi.turner@mobileye.com>
2026-07-09 21:35:34 +0300

    feat: shared palette, particle bursts, screen-shake, and sfx/music primitives

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const PALETTE = {
+  ground: 0x262638,
+  player: 0x66aaff,
+  accentGold: 0xffe066,
+  accentPink: 0xff6ec7,
+  accentGreen: 0x5cffb0,
+  accentRed: 0xff5c8a,
+  accentCyan: 0x5ce1ff,
+  accentPurple: 0xb266ff,
+  neutralLight: 0xdddddd,
+}
